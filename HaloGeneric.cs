d7390f129fbecc06e22be17fb82f1f059d86195d
Muhammad Risjad Shidqi Febian <mrisjads@gmail.com>
2023-03-19 23:43:59 +0700

    Menambahkan class HaloGeneric dengan method SapaUser

---
+++ HaloGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMOD5_1302213045
+{
+    internal class HaloGeneric
+    {
+        public void Sapauser<T>(T username)
+        {
+            Console.WriteLine("Hello user " + username + " !!!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TPMOD5_1302213045
+{
+    class mainProgram
+    {
+        static void Main(String[] args)
+        {
+            HaloGeneric haloGeneric = new HaloGeneric();
+            haloGeneric.Sapauser("Risjad");
+        }
+    }
+}
